fbf03f6b6ed6f2f5d0ab024ead3d2aca3fa3c16e
Grant Tepper <grant.tepper@gmail.com>
2017-01-08 01:52:39 -0800

    implementing sockets

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
+var EVENT_REMOVED = 'eqt:timer:removed';
+var EVENT_STARTED = 'eqt:timer:started';
+var EVENT_ADDED = 'eqt:timer:added';
+
+
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on(EVENT_REMOVED, function(data){
+    console.log('event removed recieved', data);
+    io.emit(EVENT_REMOVED, data);
+  });
+
+  socket.on(EVENT_STARTED, function(data){
+    console.log('event started recieved', data);
+    io.emit(EVENT_STARTED, data);
+
+  });
+
+  socket.on(EVENT_ADDED, function(data){
+    console.log('event add recieved', data);
+    io.emit(EVENT_ADDED, data);
+  });
 });
 
-io.on('event', function(data){
-  
-});
 
-io.on('disconnect', function(){
-  
+io.on('disconnect', function(socket){
+  console.log('socket connection closed');
 });
 
 http.listen(3000, function(){
